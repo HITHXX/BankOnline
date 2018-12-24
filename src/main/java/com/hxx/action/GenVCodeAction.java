4a9aebb6f0323858a549a909d8397f888cfa9a99
HXX <1029796949@qq.com>
2018-12-25 01:08:40 +0800

    InitVersion 1.1

--- src/main/java/com/hxx/action/GenVCodeAction.java
+++ src/main/java/com/hxx/action/GenVCodeAction.java
@@ -56,8 +56,8 @@ public class GenVCodeAction extends ActionSupport {
         final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
 //替换成你的AK
-        final String accessKeyId = "";//你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "";//你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "xxxxx";//你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "xxxxx";//你的accessKeySecret，参考本文档步骤2
 //初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                 accessKeySecret);
@@ -70,9 +70,9 @@ public class GenVCodeAction extends ActionSupport {
         //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式；发送国际/港澳台消息时，接收号码格式为国际区号+号码，如“85200000000”
         request.setPhoneNumbers(user);
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("");
+        request.setSignName("xxxxx");
         //必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
-        request.setTemplateCode("");
+        request.setTemplateCode("xxxxx");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
         String verifyCode = String.valueOf(new Random().nextInt(899999) + 100000);
